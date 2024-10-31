2c86018b7c96cff029c403226a1fe10c55e717ff
mralikeshtkar <alikeshtkar262@gmail.com>
2024-10-31 19:31:57 +0100

    some changes in kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('queue:work --timeout=60 --tries=1 --once')
-            ->everyMinute();
+            ->everyMinute()
+            ->withoutOverlapping();
     }
 
     /**
