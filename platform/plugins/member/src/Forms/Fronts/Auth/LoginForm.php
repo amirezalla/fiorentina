4cffb2a29f508cc29fbc31d48ac61064a3d01a0b
mralikeshtkar <alikeshtkar262@gmail.com>
2024-11-02 11:57:23 +0100

    add clouedflare recapcha

--- platform/plugins/member/src/Forms/Fronts/Auth/LoginForm.php
+++ platform/plugins/member/src/Forms/Fronts/Auth/LoginForm.php
@@ -79,7 +79,7 @@ class LoginForm extends AuthForm
                 'recaptcha',
                 HtmlField::class,
                 [
-                    'component' => '<div class="cf-turnstile" data-sitekey="'.config('turnstile.key').'" data-callback="onTurnstileSuccess"></div>',
+                    'html' => '<div class="cf-turnstile" data-sitekey="'.config('turnstile.key').'" data-callback="onTurnstileSuccess"></div>',
                 ]
             )
             ->add('closeRow', HtmlField::class, [
