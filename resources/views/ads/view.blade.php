5ad8a0921b69270bd4a1567eb01ad42ebc0cb01b
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-11 12:27:34 +0100

    Update view.blade.php

--- resources/views/ads/view.blade.php
+++ resources/views/ads/view.blade.php
@@ -7,43 +7,51 @@
         </div>
         <table class="table table-striped">
             <thead>
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>type</th>
-                <th>group</th>
-                <th>image</th>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>type</th>
+                    <th>group</th>
+                    <th>image</th>
+                    <th>Status</th>
 
-                <th>Actions</th>
-            </tr>
+                    <th>Actions</th>
+                </tr>
             </thead>
             <tbody>
-            @foreach($ads as $ad)
-                <tr>
-                    <td class="align-middle">{{ $ad->id }}</td>
-                    <td class="align-middle">{{ $ad->title }}</td>
-                    <td class="align-middle">{{ $ad->type }}</td>
-                    <td class="align-middle">{{ $ad->group_name }}</td>
-                    <td class="align-middle">
-                        @if($ad->getImageUrl())
-                            <img src="{{ $ad->getImageUrl() }}" width="140" alt="{{ $ad->title }}">
-                        @endif
-                    </td>
+                @foreach ($ads as $ad)
+                    <tr>
+                        <td class="align-middle">{{ $ad->id }}</td>
+                        <td class="align-middle">{{ $ad->title }}</td>
+                        <td class="align-middle">{{ $ad->type }}</td>
+                        <td class="align-middle">{{ $ad->group_name }}</td>
+                        <td class="align-middle">
+                            @if ($ad->getImageUrl())
+                                <img src="{{ $ad->getImageUrl() }}" width="140" alt="{{ $ad->title }}">
+                            @endif
+                        </td>
+                        <td>
+                            @if ($ad->status)
+                                Published
+                            @else
+                                Draft
+                            @endif
+                        </td>
 
 
 
-                    <td class="align-middle">
-                        <div class="d-flex gap-2">
-                            <a href="{{ route('ads.edit',$ad->id) }}" class="btn btn-primary">Edit</a>
-                            <form action="{{ route('ads.destroy',$ad->id) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
+                        <td class="align-middle">
+                            <div class="d-flex gap-2">
+                                <a href="{{ route('ads.edit', $ad->id) }}" class="btn btn-primary">Edit</a>
+                                <form action="{{ route('ads.destroy', $ad->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
         <div class="w-100">
