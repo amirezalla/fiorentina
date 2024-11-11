9e0750279d0a4807038232377ba8845623b52ded
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-11-11 11:01:23 +0100

    ehey

--- platform/packages/seo-helper/resources/lang/en/seo-helper.php
+++ platform/packages/seo-helper/resources/lang/en/seo-helper.php
@@ -6,7 +6,7 @@ return [
     'default_description' => 'Setup meta title & description to make your site easy to discovered on search engines such as Google',
     'seo_title' => 'SEO Title',
     'seo_description' => 'SEO description',
-    'seo_image' => 'SEO image',
+    'seo_image' => 'Immagine SEO: (Scegli un immagine pertinente e accattivante che rappresenti il contenuto della pagina. Assicurati che limmagine abbia un attributo alt descrittivo e correlato alla parola chiave principale.)',
     'allow_index' => 'Allow search engines to index this page',
     'index' => 'Index',
     'noindex' => 'No index',
