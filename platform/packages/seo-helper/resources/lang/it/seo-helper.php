7f2d1af6895a5e7cbb49dc61c815f7da3fb5723f
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2024-11-11 11:03:09 +0100

    qwr

--- platform/packages/seo-helper/resources/lang/it/seo-helper.php
+++ platform/packages/seo-helper/resources/lang/it/seo-helper.php
@@ -6,7 +6,8 @@ return [
     'default_description' => 'Imposta il titolo e la descrizione meta per rendere il tuo sito facilmente scoperto sui motori di ricerca come Google',
     'seo_title' => 'Titolo SEO',
     'seo_description' => 'Descrizione SEO',
-    'seo_image' => 'Immagine SEO',
+
+    'seo_image' => 'Immagine SEO: (Scegli un immagine pertinente e accattivante che rappresenti il contenuto della pagina. Assicurati che limmagine abbia un attributo alt descrittivo e correlato alla parola chiave principale.)',
     'allow_index' => 'Permetti ai motori di ricerca di indicizzare questa pagina',
     'index' => 'Indicizza',
     'noindex' => 'Non indicizzare',
