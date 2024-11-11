cd92af508602443119100a9ae8b16a0e11a5c31a
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-11 12:37:46 +0100

    Update AdController.php

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -134,6 +134,7 @@ class AdController extends BaseController
         try {
             // Update the advertisement
             $ad->update($data);
+            dd($ad);
             return redirect()->route('ads.index')->with('success', 'Advertisement updated successfully!');
         } catch (\Exception $e) {
             Log::error('Failed to save advertisement: ' . $e->getMessage());
