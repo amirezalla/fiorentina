5a7361c0687faa8c40f00a8be3ec0d873a9f6725
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-19 15:29:30 +0100

    778

--- resources/views/ads/includes/adsrecentp2.blade.php
+++ resources/views/ads/includes/adsrecentp2.blade.php
@@ -1,17 +1,16 @@
-@if(isset($ad) && $ad)
-@if ($ad->type == 1)
-<div class="row justify-content-center mx-0">
+@if (isset($ad) && $ad)
+    @if ($ad->type == 1)
+        <div class="row justify-content-center mx-0">
 
-    <div class="col-12 mx-auto">
-        <a href="" class="d-block">
-            <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
-                style="width: 100%; height: auto;">
-        </a>
-    </div>
+            <div class="col-12 mx-auto">
+                <a href="" class="d-block">
+                    <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
+                        style="width: 100%; height: auto;">
+                </a>
+            </div>
 
-</div>
-@else
-<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-6741446998584415"
-    data-ad-slot="{{ $ad->image }}" data-ad-format="auto" data-full-width-responsive="true"></ins>
-@endif
+        </div>
+    @else
+        {!! $ad->amp !!}
+    @endif
 @endif
