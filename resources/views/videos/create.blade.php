43ba11af58fe0f4a1450f30d0d0397b1d23907aa
mralikeshtkar <alikeshtkar262@gmail.com>
2024-11-20 10:08:43 +0100

    add order select into videos

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -118,7 +118,9 @@
                 filter: "video",
                 onSelectFiles: function (e, t) {
                     const container = $('#videoPreviewContainer');
+                    const videoPreviewItems = container.find('.video-preview-item');
                     e.forEach((i, k) => {
+                        console.log(videoPreviewItems);
                         const html = `
                         <div class="col-12 col-md-6 col-lg-4 mb-3 video-preview-item">
 
