14b7d0fe79dfc056ffb45bb94efbf6eac838c2d2
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-24 00:28:06 +0100

    Update recent-posts.blade.php

--- platform/themes/ripple/partials/shortcodes/recent-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/recent-posts.blade.php
@@ -60,7 +60,7 @@
                                                 </header>
                                                 <div class="post__content">
                                                     <p style="margin: 10px 0 0;">{{ $post->description }}</p>
-                                                    <span class="mt-3 d-block">
+                                                    <span class=" text-dark mt-3 d-block">
                                                         @php
                                                             $post->comments_count = FriendsOfBotble\Comment\Models\Comment::where(
                                                                 'reference_id',
