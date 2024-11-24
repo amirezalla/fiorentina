c8985197e297ce552569324f43cc259e06690399
mralikeshtkar <alikeshtkar262@gmail.com>
2024-11-24 10:57:07 +0100

    last post editoriale

--- resources/views/last_post_editoriale.blade.php
+++ resources/views/last_post_editoriale.blade.php
@@ -4,14 +4,24 @@
     }
 
     .editoriale-item .editoriale-item-head{
-        padding: 0 8px;
         border-bottom: 1px solid #eeeeee;
+        position: relative;
     }
 
     .editoriale-item .editoriale-item-head .editoriale-item-head-title{
         font-size: 18px;
         font-weight: 500;
         color: #111111;
+        padding: 0 8px;
+    }
+
+    .editoriale-item .editoriale-item-head .editoriale-item-head-title::before{
+        height: 2px;
+        left: 0;
+        right: 0;
+        top: 100%;
+        position: absolute;
+        background-color: red;
     }
 </style>
 <div class="w-full editoriale-item">
