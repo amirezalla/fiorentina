e86df22aa8047270f783d5688ff18189e4aba3ba
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-25 13:04:48 +0100

    mp15ed

--- resources/views/ads/includes/MOBILE_POSIZIONE_1.blade.php
+++ resources/views/ads/includes/MOBILE_POSIZIONE_1.blade.php
@@ -5,7 +5,11 @@
             <div class="col-12 mx-auto">
                 <a href="" class="d-block">
                     <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
+                    @if(!$post->width)
                         style="width: 100%; height: auto;">
+                    @else
+                        style="width: {{ $post->width }}px; height: {{ $post->height }}px;">
+                    @endif
                 </a>
             </div>
 
--- resources/views/ads/includes/MOBILE_POSIZIONE_5.blade.php
+++ resources/views/ads/includes/MOBILE_POSIZIONE_5.blade.php
@@ -5,8 +5,10 @@
             <div class="col-12 mx-auto">
                 <a href="" class="d-block">
                     <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
-                        style="width: 100%; height: auto;">
-                </a>
+                        @if (!$post->width) style="width: 100%; height: auto;">
+                    @else
+                        style="width: {{ $post->width }}px; height: {{ $post->height }}px;"> @endif
+                        </a>
             </div>
 
         </div>
