2e790f9ec11b9b49690a86577481180d8baf0243
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-25 13:23:24 +0100

    Update breadcrumbs.blade.php

--- platform/themes/ripple/partials/breadcrumbs.blade.php
+++ platform/themes/ripple/partials/breadcrumbs.blade.php
@@ -1,6 +1,4 @@
-<section
-    data-background="{{ Theme::get('breadcrumbBannerImage') ?: (theme_option('default_breadcrumb_banner_image') ? RvMedia::getImageUrl(theme_option('default_breadcrumb_banner_image')) : Theme::asset()->url('images/1275062.png')) }}"
-    class="section page-intro pt-50 pb-50 bg-cover">
+<section class="section page-intro pt-50 pb-50 bg-cover">
     <div style="opacity: 0.6" class="bg-overlay"></div>
     <div class="container">
         <h3 class="page-intro__title">{{ Theme::get('section-name') ?: SeoHelper::getTitle() }}</h3>
