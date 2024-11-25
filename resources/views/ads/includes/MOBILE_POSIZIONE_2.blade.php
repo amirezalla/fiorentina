f9fdabb9a3b1492d98c2f206d2d7d7885699edfb
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-25 12:56:11 +0100

    mp15

--- resources/views/ads/includes/MOBILE_POSIZIONE_2.blade.php
+++ resources/views/ads/includes/MOBILE_POSIZIONE_2.blade.php
@@ -1,1 +1,16 @@
-gooogleads
+@if (isset($ad) && $ad)
+    @if ($ad->type == 1)
+        <div class="row justify-content-center mx-0">
+
+            <div class="col-12 mx-auto">
+                <a href="" class="d-block">
+                    <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
+                        style="width: 100%; height: auto;">
+                </a>
+            </div>
+
+        </div>
+    @else
+        {!! $ad->amp !!}
+    @endif
+@endif
