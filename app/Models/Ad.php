9c3d140602709261ddbae59261fad842c2694a3b
mralikeshtkar <alikeshtkar262@gmail.com>
2024-11-25 11:52:54 +0100

    Add some regex change

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -206,7 +206,7 @@ class Ad extends BaseModel
                 })->flatten();
                 if ($shortCodes->count()) {
                     $adsBackground = $shortCodes->first(function ($item) use ($adsBackgroundShortCodeRegex) {
-                        dd($item);
+                        dd($item,preg_match($item, $adsBackgroundShortCodeRegex));
                         return preg_match($item, $adsBackgroundShortCodeRegex);
                     });
                     if ($adsBackground) {
