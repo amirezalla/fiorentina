c9387fb8b775aea798dcb43fd4f45aa8defd60df
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-26 14:08:15 +0100

    Update navbar-nav.blade.php

--- platform/core/base/resources/views/layouts/partials/navbar-nav.blade.php
+++ platform/core/base/resources/views/layouts/partials/navbar-nav.blade.php
@@ -1,5 +1,5 @@
 <ul @class(['navbar-nav', $navbarClass ?? null])>
-    @dd(DashboardMenu::getAll())
+
     @foreach (DashboardMenu::getAll() as $menu)
         @if (
             $menu['id'] == 'cms-core-plugins' ||
