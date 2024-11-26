0c132ff39e93b4be0c07670fc7b1cd64d562fe23
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-26 14:04:00 +0100

    Update view.blade.php

--- resources/views/diretta/view.blade.php
+++ resources/views/diretta/view.blade.php
@@ -4,11 +4,16 @@
     if ($matchId) {
         $match = Calendario::where('match_id', $matchId)->first();
         $commentaries = MatchCommentary::where('match_id', $matchId)
+            ->where(function ($query) {
+                $query->whereNotNull('comment_time')->orWhereNotNull('comment_class')->orWhereNotNull('comment_text');
+            })
             ->orderByRaw(
                 "
-        CAST(SUBSTRING_INDEX(comment_time, '+', 1) AS UNSIGNED) ASC,
-        CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(comment_time, '+', -1), \"'\", 1) AS UNSIGNED) ASC,
-        created_at ASC
+        CAST(SUBSTRING_INDEX(comment_time, \"'\", 1) AS UNSIGNED) + 
+        IF(LOCATE('+', comment_time) > 0, 
+            CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(comment_time, \"'\", 1), '+', -1) AS UNSIGNED), 
+            0
+        )
     ",
             )
             ->get();
