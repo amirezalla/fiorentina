0eb80bc76c8fb4bc0406efc7b6f8f60de05dbd55
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-26 13:49:16 +0100

    Update view.blade.php

--- resources/views/diretta/view.blade.php
+++ resources/views/diretta/view.blade.php
@@ -3,7 +3,15 @@
     use App\Models\MatchCommentary;
     if ($matchId) {
         $match = Calendario::where('match_id', $matchId)->first();
-        $commentaries = MatchCommentary::where('match_id', $matchId)->get();
+        $commentaries = MatchCommentary::where('match_id', $matchId)
+            ->orderByRaw(
+                "
+        CAST(SUBSTRING_INDEX(comment_time, '+', 1) AS UNSIGNED) ASC,
+        CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(comment_time, '+', -1), \"'\", 1) AS UNSIGNED) ASC,
+        created_at ASC
+    ",
+            )
+            ->get();
     }
 @endphp
 @extends(BaseHelper::getAdminMasterLayoutTemplate())
