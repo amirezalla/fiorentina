62fb6ebb718a72ec594dcc28987fc36d6d1fa435
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-11-26 12:25:56 +0100

    Update last_post_editoriale.blade.php

--- resources/views/last_post_editoriale.blade.php
+++ resources/views/last_post_editoriale.blade.php
@@ -1,14 +1,15 @@
-@if($last_post)
+@if ($last_post)
     <div class="w-full editoriale-item mt-30">
         <div class="editoriale-item-head d-flex">
-            <span class="editoriale-item-head-title">Editoriale</span>
+            <span class="editoriale-item-head-title">EDITORIALE</span>
         </div>
         <div class="editoriale-item-content p-2">
-            <div class="w-full d-block" >
+            <div class="w-full d-block">
                 <a href="{{ $last_post->url }}" title="{{ $last_post->name }}" class="d-block w-100">
                     {{ RvMedia::image($last_post->image, $last_post->name, 'large') }}
                 </a>
-                <a href="{{ $last_post->url }}" title="{{ $last_post->name }}" class="editoriale-item-content-title p-2">{{ $last_post->name }}</a>
+                <a href="{{ $last_post->url }}" title="{{ $last_post->name }}"
+                    class="editoriale-item-content-title p-2">{{ $last_post->name }}</a>
             </div>
         </div>
     </div>
