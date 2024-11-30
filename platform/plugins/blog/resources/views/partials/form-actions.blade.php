9334de4e780fe994ec8c4b546b84be7d1be94247
mralikeshtkar <alikeshtkar262@gmail.com>
2024-11-30 13:39:02 +0100

    add preview modal

--- platform/plugins/blog/resources/views/partials/form-actions.blade.php
+++ platform/plugins/blog/resources/views/partials/form-actions.blade.php
@@ -40,7 +40,7 @@
 </div>
 </header>
 <div class="modal fade" id="previewModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
