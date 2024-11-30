d30b5c9fc2f68482a7217621aac0ed03206931e4
mralikeshtkar <alikeshtkar262@gmail.com>
2024-11-30 15:41:03 +0100

    add preview modal

--- platform/plugins/blog/resources/views/partials/form-actions.blade.php
+++ platform/plugins/blog/resources/views/partials/form-actions.blade.php
@@ -71,7 +71,7 @@
                     url : "https://laviola.collaudo.biz/humanoid-robots-in-everyday-life-ai-companions-and-assistants",
                     success : function(response){
                         var doc = (new DOMParser).parseFromString(response, "text/html");
-                        console.log(doc)
+                        console.log(doc.querySelector('body'))
                         /*iframe.attr("srcdoc", response);
                         previewModal.modal('show');
                         console.log(document.getElementById("iframe").contentWindow.document.getElementsByTagName("*"))*/
