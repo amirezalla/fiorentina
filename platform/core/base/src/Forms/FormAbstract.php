5d8fed6524ac7e8f4d70f078c06dffc7ca1ee196
mralikeshtkar <alikeshtkar262@gmail.com>
2024-11-30 12:44:15 +0100

    get single post route name

--- platform/core/base/src/Forms/FormAbstract.php
+++ platform/core/base/src/Forms/FormAbstract.php
@@ -182,6 +182,7 @@ abstract class FormAbstract extends Form implements ExtensibleContract
 
     public function getActionButtons(): string
     {
+        dd($this->actionButtons);
         if ($this->actionButtons === '') {
             return view('core/base::forms.partials.form-actions')->render();
         }
