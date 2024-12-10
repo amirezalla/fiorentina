c26b970632b58c8bdfdcbcc18fdf679a6c51a232
mralikeshtkar <alikeshtkar262@gmail.com>
2024-12-10 15:58:51 +0100

    add weight input to vuew

--- resources/views/ads/view.blade.php
+++ resources/views/ads/view.blade.php
@@ -5,61 +5,73 @@
         <div class="mb-3">
             <a href="{{ route('ads.create') }}" class="btn btn-primary">Crea</a>
         </div>
+        <form action="" method="get">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <label for="advads-group-id" class="form-label">Gruppo annunci</label>
+                    <select class="form-select" name="group" id="advads-group-id">
+                        @foreach (\App\Models\Ad::GROUPS as $key => $title)
+                            <option value="{{ $key }}" @selected(request('group') == $key)>
+                                {{ $title }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </form>
         <table class="table table-striped">
             <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>type</th>
-                    <th>group</th>
-                    <th>image</th>
-                    <th>Weight</th>
-                    <th>Status</th>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>type</th>
+                <th>group</th>
+                <th>image</th>
+                <th>Weight</th>
+                <th>Status</th>
 
-                    <th>Actions</th>
-                </tr>
+                <th>Actions</th>
+            </tr>
             </thead>
             <tbody>
-                @foreach ($ads as $ad)
-                    <tr>
-                        <td class="align-middle">{{ $ad->id }}</td>
-                        <td class="align-middle">{{ $ad->title }}</td>
-                        <td class="align-middle">
-                            @if ($ad->type == 1)
-                                IMAGE/URL
-                            @else
-                                GOOGLE ADS
-                            @endif
-                        </td>
-                        <td class="align-middle">{{ $ad->group_name }}</td>
-                        <td class="align-middle">
-                            @if ($ad->getImageUrl())
-                                <img src="{{ $ad->getImageUrl() }}" width="140" alt="{{ $ad->title }}">
-                            @endif
-                        </td>
-                        <td>%{{ $ad->getWeightPercentage() }}</td>
-                        <td>
-                            @if ($ad->status)
-                                Published
-                            @else
-                                Draft
-                            @endif
-                        </td>
+            @foreach ($ads as $ad)
+                <tr>
+                    <td class="align-middle">{{ $ad->id }}</td>
+                    <td class="align-middle">{{ $ad->title }}</td>
+                    <td class="align-middle">
+                        @if ($ad->type == 1)
+                            IMAGE/URL
+                        @else
+                            GOOGLE ADS
+                        @endif
+                    </td>
+                    <td class="align-middle">{{ $ad->group_name }}</td>
+                    <td class="align-middle">
+                        @if ($ad->getImageUrl())
+                            <img src="{{ $ad->getImageUrl() }}" width="140" alt="{{ $ad->title }}">
+                        @endif
+                    </td>
+                    <td>{{ $ad->getWeightPercentage() }}%</td>
+                    <td>
+                        @if ($ad->status)
+                            Published
+                        @else
+                            Draft
+                        @endif
+                    </td>
 
 
-
-                        <td class="align-middle">
-                            <div class="d-flex gap-2">
-                                <a href="{{ route('ads.edit', $ad->id) }}" class="btn btn-primary">Edit</a>
-                                <form action="{{ route('ads.destroy', $ad->id) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
+                    <td class="align-middle">
+                        <div class="d-flex gap-2">
+                            <a href="{{ route('ads.edit', $ad->id) }}" class="btn btn-primary">Edit</a>
+                            <form action="{{ route('ads.destroy', $ad->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
         <div class="w-100">
