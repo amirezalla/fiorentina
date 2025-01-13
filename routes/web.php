dfbe146eb67cca2eb6d36f22dac8a976631a5e8e
Amirezalla <allahverdiamirreza@gmail.com>
2025-01-13 11:26:48 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -131,8 +131,7 @@ Route::post('/store-commentary', [DirettaController::class, 'storeCommentary'])-
 
 Route::get('/check-db-connection', function () {
         // Attempt to connect to the mysql2 database
-        $connection = DB::connection('mysql2');
-        $connection->select('SELECT 1'); 
-        dd($connection);
+        $users = DB::connection('mysql2')->table('frntn_users')->get();
+        dd($users);
 
 });
