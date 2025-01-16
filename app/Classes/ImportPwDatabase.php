d6f6951526b043069016b027350023df1d4c01a9
mralikeshtkar <alikeshtkar262@gmail.com>
2025-01-16 17:29:59 +0100

    Update ImportPwDatabase.php

--- app/Classes/ImportPwDatabase.php
+++ app/Classes/ImportPwDatabase.php
@@ -16,7 +16,15 @@ class ImportPwDatabase
         $max_page = ceil($usersCount / 100);
         dump($usersCount);
         foreach (range(0, $max_page - 1) as $i) {
-            dump($i * 100);
+            $o = $i * 100;
+            $users = DB::connection('mysql2')
+                ->table("frntn_users")
+                ->skip($o)
+                ->limit(100)
+                ->get()
+                ->map(fn($i) => json_decode(json_encode($i), true))
+                ->toArray();
+            dd($users);
 //            ImportUserFromWpUsersDatabase::dispatch($i * 100);
         }
     }
