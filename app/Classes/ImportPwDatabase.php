415280a48102c3217e357b3c180e72b1c5298d63
mralikeshtkar <alikeshtkar262@gmail.com>
2025-01-16 17:31:38 +0100

    Update ImportPwDatabase.php

--- app/Classes/ImportPwDatabase.php
+++ app/Classes/ImportPwDatabase.php
@@ -3,7 +3,9 @@
 namespace App\Classes;
 
 use App\Jobs\ImportUserFromWpUsersDatabase;
+use Botble\ACL\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class ImportPwDatabase
 {
@@ -20,10 +22,26 @@ class ImportPwDatabase
             $users = DB::connection('mysql2')
                 ->table("frntn_users")
                 ->skip($o)
-                ->limit(100)
+                ->limit(5)
                 ->get()
                 ->map(fn($i) => json_decode(json_encode($i), true))
                 ->toArray();
+            foreach ($users as $user) {
+                $firstName = trim(Str::before($user['display_name'], " "));
+                $lastName = trim(Str::after($user['display_name'], " "));
+                User::unguard();
+                User::query()->create([
+                    'id' => $user['ID'],
+                    'email' => $user['user_email'],
+                    'username' => $user['user_nicename'],
+                    'password' => $user['user_pass'],
+                    'first_name' => strlen($firstName) ? $firstName : null,
+                    'last_name' => strlen($lastName) ? $lastName : null,
+                    'created_at' => $user['user_registered'],
+                    'email_verified_at' => now(),
+                ]);
+                User::reguard();
+            }
             dd($users);
 //            ImportUserFromWpUsersDatabase::dispatch($i * 100);
         }
