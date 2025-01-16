d732405754237a090989dc09fa3744b9a0a1a357
mralikeshtkar <alikeshtkar262@gmail.com>
2025-01-16 17:55:11 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,8 @@ use App\Http\Controllers\VideoController;
 
 Route::get('/migrate', function (\Illuminate\Http\Request $request) {
     $importPwDatabase = new \App\Classes\ImportPwDatabase();
-//    $importPwDatabase->importUsers();
     $importPwDatabase->importUsers();
+//    $importPwDatabase->importPosts();
     dd("ok");
     $tables = collect(DB::connection('mysql2')->select('SHOW TABLES'))->map(fn($i) => $i->Tables_in_fiorentina)->toArray();
     $result = [];
