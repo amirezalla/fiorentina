11af8fe60e17d3cc4c5981e4d570eb28e070be3a
mralikeshtkar <alikeshtkar262@gmail.com>
2025-01-16 17:05:06 +0100

    Update ImportUserFromWpUsersDatabase.php

--- app/Jobs/ImportUserFromWpUsersDatabase.php
+++ app/Jobs/ImportUserFromWpUsersDatabase.php
@@ -49,7 +49,7 @@ class ImportUserFromWpUsersDatabase implements ShouldQueue
                 'password' => $user['user_pass'],
                 'first_name' => strlen($firstName) ? $firstName : null,
                 'last_name' => strlen($lastName) ? $lastName : null,
-                'created_at' => $user['user_registered'],
+                'created_at' => now()->subYears(5),
             ]);
         }
     }
