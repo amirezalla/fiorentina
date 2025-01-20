e4f9b855ab9b1e021ba8e1dd4d2d8262f85510bc
Amirezalla <allahverdiamirreza@gmail.com>
2025-01-20 11:18:06 +0100

    1118

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,5 @@ Route::post('/store-commentary', [DirettaController::class, 'storeCommentary'])-
 
 Route::get('/import-users-wp', [WpImportController::class, 'users']);
 Route::get('/import-single-post-wp', [WpImportController::class, 'singlePost']);
-Route::get('/import-categories', [WpImportController::class, 'categories']);
 
 
