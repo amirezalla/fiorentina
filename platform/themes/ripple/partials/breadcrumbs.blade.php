207a96c181a92f15ff8fcbab5c4a95fe7e934d8d
Amirezalla <allahverdiamirreza@gmail.com>
2025-01-20 08:25:46 +0100

    Update breadcrumbs.blade.php

--- platform/themes/ripple/partials/breadcrumbs.blade.php
+++ platform/themes/ripple/partials/breadcrumbs.blade.php
@@ -1,7 +1,7 @@
-<section class="section page-intro pt-30 pb-30 bg-cover">
+{{-- <section class="section page-intro pt-30 pb-30 bg-cover">
     <div style="opacity: 0.6" class="bg-overlay"></div>
     <div class="container">
         <h3 class="page-intro__title">{{ Theme::get('section-name') ?: SeoHelper::getTitle() }}</h3>
         {!! Theme::breadcrumb()->render() !!}
     </div>
-</section>
+</section> --}}
