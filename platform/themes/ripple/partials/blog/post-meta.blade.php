58daf71817a4b8bcf052c4e694c8aa2b7488fbcd
Amirezalla <allahverdiamirreza@gmail.com>
2025-01-20 10:13:23 +0100

    Update post-meta.blade.php

--- platform/themes/ripple/partials/blog/post-meta.blade.php
+++ platform/themes/ripple/partials/blog/post-meta.blade.php
@@ -18,7 +18,7 @@
             @else
                 <span class="post-author " style="color: gray;">{!! BaseHelper::renderIcon('ti ti-user-circle') !!}
             @endif
-            <span class="author-name">{{ $post->author->first_name }} {{ $post->author->last_name }}</span>
+            <span class="author-name">{{ $post->author->name }}</span>
 
         </div>
         <div class="col-lg-5 d-flex justify-content-end pr-30" style="padding-bottom: 14px">
