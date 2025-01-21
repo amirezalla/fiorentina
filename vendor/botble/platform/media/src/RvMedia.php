65edbf23b24c48ddb38cda3f0f5f11aec6b021c6
Amirezalla <allahverdiamirreza@gmail.com>
2025-01-21 17:50:19 +0100

    Update RvMedia.php

--- vendor/botble/platform/media/src/RvMedia.php
+++ vendor/botble/platform/media/src/RvMedia.php
@@ -246,8 +246,9 @@ class RvMedia
 
             return str_replace('.digitaloceanspaces.com', '.cdn.digitaloceanspaces.com', Storage::url($path));
         }
+        return Storage::temporaryUrl($path, now()->addMinutes(15)); // Adjust expiration time as needed
 
-        return Storage::url($path);
+        // return Storage::url($path);
     }
 
     public function getDefaultImage(bool $relative = false, ?string $size = null): string
