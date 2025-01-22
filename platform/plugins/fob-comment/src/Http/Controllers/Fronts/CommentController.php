3d599988ad50a68cabc8b7de01faacde2125c76c
mralikeshtkar <alikeshtkar262@gmail.com>
2025-01-22 15:54:36 +0100

    Update CommentController.php

--- platform/plugins/fob-comment/src/Http/Controllers/Fronts/CommentController.php
+++ platform/plugins/fob-comment/src/Http/Controllers/Fronts/CommentController.php
@@ -110,7 +110,7 @@ class CommentController extends BaseController
         $comment = Comment::query()
             ->where('status', CommentStatus::APPROVED)
             ->findOrFail($comment);
-        dd($request->user(),$request->user('web'),auth()->user(),auth('web')->user());
+        dd($request->user(),$request->user('member'),auth()->user(),auth('web')->user());
         if (is_null($request->user())) {
             return response()->json([
                 'message' => "Unauthenticated.",
