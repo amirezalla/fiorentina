64ccfd27a82456fc45b8fab02beab07647f66aba
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-06 22:43:32 +0100

    Update category.blade.php

--- platform/themes/ripple/views/category.blade.php
+++ platform/themes/ripple/views/category.blade.php
@@ -6,20 +6,16 @@
             <div class="post__thumbnail">
                 {{ RvMedia::image($post->image, $post->name, 'thumbnail') }}
                 <a href="{{ $post->url }}" title="{{ $post->name }}" class="post__overlay"></a>
-                <div class="post__content-wrap">
-                    <header class="post__header">
-                        <h3 class="post__title"><a href="{{ $post->url }}"
-                                title="{{ $post->name }}">{{ $post->name }}</a></h3>
-                        // <div class="post__meta">
-                            // {!! Theme::partial('blog.post-meta', compact('post')) !!}
-                            // </div>
-                    </header>
+            </div>
+            <div class="post__content-wrap">
+                <header class="post__header">
+                    <h3 class="post__title"><a href="{{ $post->url }}"
+                            title="{{ $post->name }}">{{ $post->name }}</a></h3>
+
                     <div class="post__content">
                         <p data-number-line="4">{{ $post->description }}</p>
                     </div>
-                </div>
             </div>
-
         </article>
     @endforeach
     <div class="page-pagination text-right">
