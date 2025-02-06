f4804a08afceb4dc1e18b223ec9a6a0b6d020f78
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-06 22:42:57 +0100

    Update category.blade.php

--- platform/themes/ripple/views/category.blade.php
+++ platform/themes/ripple/views/category.blade.php
@@ -6,6 +6,18 @@
             <div class="post__thumbnail">
                 {{ RvMedia::image($post->image, $post->name, 'thumbnail') }}
                 <a href="{{ $post->url }}" title="{{ $post->name }}" class="post__overlay"></a>
+                <div class="post__content-wrap">
+                    <header class="post__header">
+                        <h3 class="post__title"><a href="{{ $post->url }}"
+                                title="{{ $post->name }}">{{ $post->name }}</a></h3>
+                        // <div class="post__meta">
+                            // {!! Theme::partial('blog.post-meta', compact('post')) !!}
+                            // </div>
+                    </header>
+                    <div class="post__content">
+                        <p data-number-line="4">{{ $post->description }}</p>
+                    </div>
+                </div>
             </div>
 
         </article>
