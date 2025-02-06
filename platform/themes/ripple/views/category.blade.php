4d172d9a8b2d2ab02c7a376f325b62dcad602b6c
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-06 22:34:51 +0100

    Update category.blade.php

--- platform/themes/ripple/views/category.blade.php
+++ platform/themes/ripple/views/category.blade.php
@@ -1,31 +1,36 @@
-@if ($posts->isNotEmpty())
-    @foreach ($posts as $post)
-        <article class="post post__horizontal mb-40 clearfix">
-            <div class="post__thumbnail">
-                {{ RvMedia::image($post->image, $post->name, 'medium') }}
-                <a class="post__overlay" href="{{ $post->url }}" title="{{ $post->name }}"></a>
-            </div>
-            <div class="post__content-wrap">
-                <header class="post__header">
-                    <h3 class="post__title"><a href="{{ $post->url }}"
-                            title="{{ $post->name }}">{{ $post->name }}</a></h3>
-                    <div class="post__meta">
-                        {!! Theme::partial('blog.post-meta', compact('post')) !!}
-                    </div>
-                </header>
-                <div class="post__content p-0">
-                    <p data-number-line="4">{{ $post->description }}</p>
-                </div>
-            </div>
-        </article>
-    @endforeach
-    <div class="page-pagination text-right">
-        {!! $posts->withQueryString()->links() !!}
-    </div>
-@endif
+@php Theme::set('section-name', $category->name) @endphp
 
-<style>
-    .section.pt-50.pb-100 {
-        background-color: #ecf0f1;
-    }
-</style>
+
+AHMAQ
+
+// @if ($posts->isNotEmpty())
+    // @foreach ($posts->loadMissing('author') as $post)
+        // <article class="post post__horizontal mb-40 clearfix">
+            // <div class="post__thumbnail">
+                // {{ RvMedia::image($post->image, $post->name, 'medium') }}
+                // <a href="{{ $post->url }}" title="{{ $post->name }}" class="post__overlay"></a>
+                // </div>
+            // <div class="post__content-wrap">
+                // <header class="post__header">
+                    // <h3 class="post__title"><a href="{{ $post->url }}" //
+                            title="{{ $post->name }}">{{ $post->name }}</a></h3>
+                    // <div class="post__meta">
+                        // {!! Theme::partial('blog.post-meta', compact('post')) !!}
+                        // </div>
+                    // </header>
+                // <div class="post__content">
+                    // <p data-number-line="4">{{ $post->description }}</p>
+                    // </div>
+                // </div>
+            // </article>
+        //
+    @endforeach
+    // <div class="page-pagination text-right">
+        // {!! $posts->links() !!}
+        // </div>
+    //
+@else
+    // <div class="alert alert-warning">
+        // <p class="mb-0">{{ __('There is no data to display!') }}</p>
+        // </div>
+    // @endif
