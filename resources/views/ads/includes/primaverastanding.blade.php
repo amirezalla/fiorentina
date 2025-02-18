febdaafc9f03038ee87399bdf6aa1e178b0516ae
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-18 10:17:29 +0100

    Update primaverastanding.blade.php

--- resources/views/ads/includes/primaverastanding.blade.php
+++ resources/views/ads/includes/primaverastanding.blade.php
@@ -1,3 +1,3 @@
 <iframe src="https://www.tuttocampo.it/WidgetV2/Classifica/ced08931-1ad2-40b4-8169-0175129df90b" width="100%"
-    frameborder="0" scrolling="no">
+    height="600" frameborder="0" scrolling="no">
 </iframe>
