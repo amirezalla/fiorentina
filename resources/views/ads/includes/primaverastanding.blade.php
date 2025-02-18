ca0b49a0e378b519d91d4191d96adb624ff46448
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-18 09:36:49 +0100

    Update primaverastanding.blade.php

--- resources/views/ads/includes/primaverastanding.blade.php
+++ resources/views/ads/includes/primaverastanding.blade.php
@@ -6,10 +6,16 @@
 
 @php
     // Create an HTTP client instance.
-    $client = new Client();
-
+    $client = new Client([
+        'headers' => [
+            'User-Agent' =>
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+            'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language' => 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7',
+        ],
+    ]);
     // Fetch the remote page.
-    $response = $client->get('https://www.diretta.it/calcio/italia/primavera-1/classifiche/#/6NcAZJet/table/overall');
+    $response = $client->get('https://www.tuttocampo.it/Italia/Primavera/GironeA/Classifica');
     $html = $response->getBody()->getContents();
 
     // Use DomCrawler to parse the HTML.
@@ -17,10 +23,10 @@
 
     // Try to extract the element with the class .table_ranking.
     try {
-        $tableRankingHtml = $crawler->filter('.tournament-table-standings')->html();
+        $tableRankingHtml = $crawler->filter('.table_ranking')->html();
     } catch (\Exception $e) {
         $tableRankingHtml = '<p>Ranking table not found.</p>';
     }
 @endphp
 
-@dd($tableRankingHtml)
+{!! $tableRankingHtml !!}
