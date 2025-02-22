3d23ccd47680ba5bc8a8d759468efdd5d6afee36
mralikeshtkar <alikeshtkar262@gmail.com>
2025-02-22 23:07:30 +0100

    add rate average

--- resources/views/ads/includes/polls.blade.php
+++ resources/views/ads/includes/polls.blade.php
@@ -30,7 +30,7 @@
         </div>
     @endforeach
 </div>
-
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
 <script>
     document.addEventListener("DOMContentLoaded",()=>{
