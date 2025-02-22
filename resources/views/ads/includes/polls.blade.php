1311380d63671374148dc5993637212c2baed92a
mralikeshtkar <alikeshtkar262@gmail.com>
2025-02-22 22:49:17 +0100

    add rate average

--- resources/views/ads/includes/polls.blade.php
+++ resources/views/ads/includes/polls.blade.php
@@ -30,6 +30,7 @@
         </div>
     @endforeach
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     document.querySelectorAll('.stars').forEach((parentElement) => {
