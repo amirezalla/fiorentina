7dda069124c601c2ddd366166ac2d62834b90994
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-23 21:35:36 +0100

    Update calendario.blade.php

--- resources/views/ads/includes/calendario.blade.php
+++ resources/views/ads/includes/calendario.blade.php
@@ -182,11 +182,7 @@
         </section>
 
 
-        <!-- jQuery (required by Bootstrap) -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 
-        <!-- Bootstrap JS -->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
 
         <script>
