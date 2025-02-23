0073ea60f247025f849fab38554ceed57b2e6a2e
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-23 23:04:12 +0100

    Update footer.blade.php

--- resources/views/emails/footer.blade.php
+++ resources/views/emails/footer.blade.php
@@ -1,4 +1,5 @@
 <!-- Customize your footer here -->
+<!-- Customize your footer here -->
 <div style="background: #441274; padding: 20px; text-align: center;">
     <p style="color:white">&copy; {{ date('Y') }} Laviola. All rights reserved.</p>
 </div>
