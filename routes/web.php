d5e593b1f32b4851724b937811869600a62e5813
mralikeshtkar <alikeshtkar262@gmail.com>
2025-02-23 11:25:50 +0100

    web.php updated

--- routes/web.php
+++ routes/web.php
@@ -163,7 +163,7 @@ Route::get('/import-categories', [WpImportController::class, 'importCategories']
 Route::get('/send-sample-email', function () {
 //    $recipient = 'allahverdiamirreza@gmail.com';
     $recipient = 'alikeshtkar262@gmail.com';
-
+    dd(config('mail.default'));
     try {
         Mail::raw('This is a sample email sent from our Laravel application.', function ($message) use ($recipient) {
             $message->to($recipient)
