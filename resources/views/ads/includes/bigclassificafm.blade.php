b72f90215b74d7e4c1874fc90fcba7ae36b76d88
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-23 18:03:27 +0100

    1

--- resources/views/ads/includes/bigclassificafm.blade.php
+++ resources/views/ads/includes/bigclassificafm.blade.php
@@ -17,33 +17,4 @@
 </iframe>
 
 
-<div class="clever-core-ads"></div>
 
-<script data-cfasync="false" type="text/javascript" id="clever-core">
-    /* <![CDATA[ */
-    (function(document, window) {
-        var a, c = document.createElement("script"),
-            f = window.frameElement;
-        console.log('clever ads executed');
-
-        c.id = "CleverCoreLoader86635";
-        c.src = "https://scripts.cleverwebserver.com/8e254b0f2bce14ae124c2fcd3bbfe197.js";
-
-        c.async = !0;
-        c.type = "text/javascript";
-        c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
-        c.setAttribute("data-callback", "put-your-callback-function-here");
-        c.setAttribute("data-callback-url-click", "put-your-click-macro-here");
-        c.setAttribute("data-callback-url-view", "put-your-view-macro-here");
-
-        try {
-            a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
-        } catch (e) {
-            a = !1;
-        }
-
-        a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
-        a.parentNode.insertBefore(c, a);
-    })(document, window);
-    /* ]]> */
-</script>
