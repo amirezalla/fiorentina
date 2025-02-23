22852759220dab7e7ef56c418abc189660e2c0da
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-23 08:49:43 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(MailManager $manager): void
     {
         URL::forceScheme('https');
 
