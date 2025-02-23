06800b62083a178289241ce0e41b72e6a599b39e
mralikeshtkar <alikeshtkar262@gmail.com>
2025-02-23 11:25:04 +0100

    web.php updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(MailManager $manager): void
     {
+        config()->set('mail.default', "sendgrid");
         URL::forceScheme('https');
 
         set_time_limit(900); // Sets the maximum execution time to 15 minutes
