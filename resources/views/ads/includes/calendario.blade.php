b67c941000a8d642f00865177c3e03aca948d0f4
Amirezalla <allahverdiamirreza@gmail.com>
2025-02-23 21:32:22 +0100

    Update calendario.blade.php

--- resources/views/ads/includes/calendario.blade.php
+++ resources/views/ads/includes/calendario.blade.php
@@ -180,8 +180,7 @@
 
 
         </section>
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 
         <!-- jQuery (required by Bootstrap) -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
