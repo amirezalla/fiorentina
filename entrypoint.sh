a88135e498847db1796716a06beac2989a86f49f
Amirezalla <allahverdiamirreza@gmail.com>
2025-03-16 09:52:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-# Fetch certificate and key from Cloud Storage
-gsutil cp gs://laviola-sql/cloudflare_origin.pem /tmp/cloudflare_origin.pem
-gsutil cp gs://laviola-sql/cloudflare_origin.key /tmp/cloudflare_origin.key
 
-# Set permissions on the key file
-chmod 600 /tmp/cloudflare_origin.key
-
-# Optionally, if Apache requires the files in /etc/ssl/certs and /etc/ssl/private,
-# move them (make sure the directories are writable or adjust Apache config accordingly)
-cp /tmp/cloudflare_origin.pem /etc/ssl/certs/cloudflare_origin.pem
-cp /tmp/cloudflare_origin.key /etc/ssl/private/cloudflare_origin.key
 # Setup Laravel Schedule Run
 echo "* * * * * cd /var/www/html && php artisan schedule:run >> /var/log/cron.log 2>&1" | crontab -
 
