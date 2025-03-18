c82ba8a524d74b8802296452ef5427520cb5cb42
Amirezalla <allahverdiamirreza@gmail.com>
2025-03-18 15:02:45 +0100

    1

--- resources/views/last_post_editoriale.blade.php
+++ resources/views/last_post_editoriale.blade.php
@@ -1,7 +1,7 @@
 @if ($last_post)
     <div class="w-full editoriale-item mt-30">
         <div class="editoriale-item-head d-flex">
-            <span class="editoriale-item-head-title">EDITORIALE</span>
+            <span class="editoriale-item-head-title px-1" style="border-bottom: 6px solid #441274">EDITORIALE</span>
         </div>
         <div class="editoriale-item-content p-2">
             <div class="w-full d-block">
@@ -9,7 +9,7 @@
                     {{ RvMedia::image($last_post->image, $last_post->name, 'large') }}
                 </a>
                 <a href="{{ $last_post->url }}" title="{{ $last_post->name }}"
-                    class="editoriale-item-content-title p-2">{{ $last_post->name }}</a>
+                    class="editoriale-item-content-title py-2" style="font-size: x-large;font-family: 'Roboto Condensed';color: black;font-weight: 700;">{{ $last_post->name }}</a>
             </div>
         </div>
     </div>
