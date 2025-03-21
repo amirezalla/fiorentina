cbd5ce0cd620528c56e7f65a82783343df0f33d5
Amirezalla <allahverdiamirreza@gmail.com>
2025-03-21 15:49:37 +0100

    Update MOBILE_DOPO_FOTO_26.blade.php

--- resources/views/ads/includes/MOBILE_DOPO_FOTO_26.blade.php
+++ resources/views/ads/includes/MOBILE_DOPO_FOTO_26.blade.php
@@ -4,7 +4,7 @@
 
             <div class="col-12 mx-auto">
                 <a href="" class="d-block">
-                    <img src="{{ $ad->getOptimizedImageUrlAttribute() }}" alt="{{ $ad->title }}" class="img-fluid"
+                    <img src="{{ $ad->getImageUrl() }}" alt="{{ $ad->title }}" class="img-fluid"
                         @if (!$ad->width) style="width: 100%; height: auto;">
                 @else
                     style="width: {{ $ad->width }}px; height: {{ $ad->height }}px;"> @endif
