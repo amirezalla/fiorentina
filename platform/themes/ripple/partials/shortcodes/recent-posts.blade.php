3d6f0fef6b405201f3a242da641e3c407e2bebd7
Amirezalla <allahverdiamirreza@gmail.com>
2025-03-21 12:16:46 +0100

    1216

--- platform/themes/ripple/partials/shortcodes/recent-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/recent-posts.blade.php
@@ -272,6 +272,9 @@
                         @include('ads.includes.SIZE_300X250_TOP')
                     </div>
                     @include('last_post_editoriale')
+                    <div class="row mt-30 ad-top-sidebar">
+                        @include('ads.includes.SIZE_300X250_B1')
+                    </div>
                     <div class="page-content">
                         <div class="post-group">
                             <div class="post-group__header">
