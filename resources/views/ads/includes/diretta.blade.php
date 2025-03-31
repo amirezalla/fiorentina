6273c850d381c8751d51ce0326fb8f0b9927f485
Amirezalla <allahverdiamirreza@gmail.com>
2025-03-31 10:37:06 +0200

    Update diretta.blade.php

--- resources/views/ads/includes/diretta.blade.php
+++ resources/views/ads/includes/diretta.blade.php
@@ -205,8 +205,12 @@
             </div>
         @endif
     </div>
-@endif
+    {{-- Diretta History blade --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+@else
+    <script>
+        window.location.href = '/404';
+    </script>
 
-{{-- Diretta History blade --}}
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+@endif
