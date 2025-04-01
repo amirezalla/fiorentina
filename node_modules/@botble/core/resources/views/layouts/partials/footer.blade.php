bcef289f0e75d07d556d065f19709e2071fcdb52
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-01 15:49:09 +0200

    1

--- node_modules/@botble/core/resources/views/layouts/partials/footer.blade.php
+++ node_modules/@botble/core/resources/views/layouts/partials/footer.blade.php
@@ -13,4 +13,50 @@
             </div>
         </div>
     </div>
+
+
+    <script>
+        $(document).on('click', '.quick-edit-btn', function() {
+            var postId = $(this).data('id');
+            var postName = $(this).data('name');
+            var $currentRow = $(this).closest('tr');
+
+            // Check if the next row is already our quick edit row for this post.
+            if ($currentRow.next().hasClass('quick-edit-row') && $currentRow.next().data('id') == postId) {
+                $currentRow.next().toggle();
+            } else {
+                // Remove any other quick edit rows, if needed.
+                $('.quick-edit-row').remove();
+
+                // Determine the number of columns in the current row.
+                var colCount = $currentRow.find('td').length;
+                // Build the quick edit form row.
+                var formRow = '<tr class="quick-edit-row" data-id="' + postId + '">' +
+                    '<td colspan="' + colCount + '">' +
+                    '<form action="' + '{{ url('posts') }}' + '/' + postId +
+                    '/quick-edit" method="POST" class="quick-edit-form">' +
+                    '{{ csrf_field() }}' +
+                    '<div class="form-group">' +
+                    '<label for="name-' + postId + '">Post Name</label>' +
+                    '<input type="text" name="name" id="name-' + postId + '" value="' + postName +
+                    '" class="form-control">' +
+                    '</div>' +
+                    '<button type="submit" class="btn btn-primary">Save</button> ' +
+                    '<button type="button" class="btn btn-secondary cancel-quick-edit" data-id="' + postId +
+                    '">Cancel</button>' +
+                    '</form>' +
+                    '</td>' +
+                    '</tr>';
+
+                // Insert the quick edit form row immediately after the current row.
+                $currentRow.after(formRow);
+            }
+        });
+
+        $(document).on('click', '.cancel-quick-edit', function() {
+            $(this).closest('tr').remove();
+        });
+    </script>
+
+
 </footer>
