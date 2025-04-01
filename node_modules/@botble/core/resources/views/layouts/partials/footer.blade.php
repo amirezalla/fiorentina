155bb33b14a232a558b8282d52bf8b5e61ee9f4e
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-01 18:38:20 +0200

    1

--- node_modules/@botble/core/resources/views/layouts/partials/footer.blade.php
+++ node_modules/@botble/core/resources/views/layouts/partials/footer.blade.php
@@ -14,7 +14,7 @@
         </div>
     </div>
     <div class="modal fade" id="quickEditModal" tabindex="-1" role="dialog" aria-labelledby="quickEditModalLabel"
-        aria-hidden="true">
+        aria-hidden="true" style="margin-top: 100px;">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -27,28 +27,68 @@
                     <form id="quickEditForm" method="POST" action="">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="hidden" id="post_id" name="id" value="">
+
+                        <!-- Name -->
                         <div class="form-group">
                             <label for="post_name">Name</label>
                             <input type="text" id="post_name" name="name" class="form-control">
                         </div>
+
+                        <!-- Slug -->
                         <div class="form-group">
                             <label for="post_slug">Slug</label>
                             <input type="text" id="post_slug" name="slug" class="form-control">
                         </div>
-                        <div class="form-group">
-                            <label for="post_date">Date</label>
-                            <input type="date" id="post_date" name="date" class="form-control">
+
+                        <!-- Data (Scheduled Publishing) -->
+                        <label>Data</label>
+                        <div class="form-row align-items-center mb-3">
+                            <!-- Day -->
+                            <div class="col-auto">
+                                <input type="number" name="day" id="post_day" class="form-control" min="1"
+                                    max="31" placeholder="01" style="width: 60px;">
+                            </div>
+                            <!-- Month -->
+                            <div class="col-auto">
+                                <select name="month" id="post_month" class="form-control">
+                                    <option value="1">Gen</option>
+                                    <option value="2">Feb</option>
+                                    <option value="3">Mar</option>
+                                    <option value="4">Apr</option>
+                                    <option value="5">Mag</option>
+                                    <option value="6">Giu</option>
+                                    <option value="7">Lug</option>
+                                    <option value="8">Ago</option>
+                                    <option value="9">Set</option>
+                                    <option value="10">Ott</option>
+                                    <option value="11">Nov</option>
+                                    <option value="12">Dic</option>
+                                </select>
+                            </div>
+                            <!-- Year -->
+                            <div class="col-auto">
+                                <input type="number" name="year" id="post_year" class="form-control"
+                                    placeholder="2025" style="width: 80px;">
+                            </div>
+                            <div class="col-auto">
+                                alle
+                            </div>
+                            <!-- Hour -->
+                            <div class="col-auto">
+                                <input type="number" name="hour" id="post_hour" class="form-control" min="0"
+                                    max="23" placeholder="17" style="width: 60px;">
+                            </div>
+                            <div class="col-auto">
+                                :
+                            </div>
+                            <!-- Minute -->
+                            <div class="col-auto">
+                                <input type="number" name="minute" id="post_minute" class="form-control"
+                                    min="0" max="59" placeholder="22" style="width: 60px;">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="post_hour">Hour</label>
-                            <input type="number" id="post_hour" name="hour" class="form-control" min="0"
-                                max="23">
-                        </div>
-                        <div class="form-group">
-                            <label for="post_minute">Minute</label>
-                            <input type="number" id="post_minute" name="minute" class="form-control" min="0"
-                                max="59">
-                        </div>
+
+                        <!-- Status -->
                         <div class="form-group">
                             <label for="post_status">Status</label>
                             <select id="post_status" name="status" class="form-control">
@@ -56,9 +96,53 @@
                                 <option value="draft">Draft</option>
                             </select>
                         </div>
-                        <!-- Optionally, you can add categories and tags fields if needed -->
+
+                        <!-- Categories (Example Static List) -->
+                        <div class="form-group">
+                            <label for="categories">Categorie</label>
+                            <div>
+                                <!-- Replace with dynamic generation as needed -->
+                                <label>
+                                    <input type="checkbox" name="categories[]" value="1">
+                                    Archivio Sondaggi
+                                </label><br>
+                                <label>
+                                    <input type="checkbox" name="categories[]" value="2">
+                                    Featured
+                                </label><br>
+                                <label>
+                                    <input type="checkbox" name="categories[]" value="3">
+                                    Fotoalbum
+                                </label><br>
+                                <label>
+                                    <input type="checkbox" name="categories[]" value="4">
+                                    Fotogallery
+                                </label><br>
+                                <label>
+                                    <input type="checkbox" name="categories[]" value="5">
+                                    Stagione 2015-16
+                                </label><br>
+                                <label>
+                                    <input type="checkbox" name="categories[]" value="6">
+                                    Stagione 2016-17
+                                </label><br>
+                                <label>
+                                    <input type="checkbox" name="categories[]" value="7">
+                                    Stagione 2017-18
+                                </label>
+                            </div>
+                        </div>
+
+                        <!-- Tags -->
+                        <div class="form-group">
+                            <label for="post_tags">Tag</label>
+                            <textarea id="post_tags" name="tags" class="form-control" placeholder="Separa i tag con delle virgole"></textarea>
+                        </div>
+
+                        <!-- Submit -->
                         <button type="submit" class="btn btn-primary">Save Changes</button>
                     </form>
+
                 </div>
             </div>
         </div>
@@ -90,7 +174,9 @@
             $('#post_minute').val(postMinute);
             $('#post_status').val(postStatus);
 
+
             // Open the modal
+            $('.modal-backdrop').remove();
             $('#quickEditModal').modal('show');
         });
     </script>
