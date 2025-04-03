9b1cded7f5f64e56b8b0ac16ce4caeaab79980a7
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-03 08:29:33 +0200

    Update CreatedAtColumn.php

--- platform/core/table/src/Columns/CreatedAtColumn.php
+++ platform/core/table/src/Columns/CreatedAtColumn.php
@@ -2,11 +2,22 @@
 
 namespace Botble\Table\Columns;
 
+use Carbon\Carbon;
+
 class CreatedAtColumn extends DateColumn
 {
     public static function make(array|string $data = [], string $name = ''): static
     {
         return parent::make($data ?: 'created_at', $name)
-            ->title(trans('core/base::tables.created_at'));
+            ->title(trans('core/base::tables.created_at'))
+            ->renderUsing(function ($value) {
+                if (!$value) {
+                    return '';
+                }
+                // Parse the date, set locale to Italian, and format as "05 Feb 2025"
+                // Note: translatedFormat will output month abbreviations according to the locale.
+                $date = Carbon::parse($value)->locale('it')->translatedFormat('d M Y');
+                return $date;
+            });
     }
 }
