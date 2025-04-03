0c49aeee21cee0f7503f9012232d06eb80ad5dec
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-03 06:39:16 +0200

    1

--- node_modules/@botble/core/resources/views/layouts/partials/footer.blade.php
+++ node_modules/@botble/core/resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,6 @@
+@php
+    use Botble\Blog\Models\Post;
+@endphp
 <footer class="footer position-sticky footer-transparent d-print-none">
     <div class="{{ AdminAppearance::getContainerWidth() }}">
         <div class="text-start">
@@ -13,148 +16,13 @@
             </div>
         </div>
     </div>
-    <div class="modal fade" id="quickEditModal" tabindex="-1" role="dialog" aria-labelledby="quickEditModalLabel"
-        aria-hidden="true" style="margin-top: 100px;">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="quickEditModalLabel">Quick Edit Post</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form id="quickEditForm" method="POST" action="">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <input type="hidden" id="post_id" name="id" value="">
+    <input type="hidden" id="post_id" name="post_id" value="">
 
-                        <!-- Name -->
-                        <div class="form-group">
-                            <label for="post_name">Name</label>
-                            <input type="text" id="post_name" name="name" class="form-control">
-                        </div>
+    @include('core/base::partials.quick_edit', [
+        'action' => route('posts.quick-edit', $post->id ?? 0),
+        'postId' => $post->id ?? '',
+    ])
 
-                        <!-- Slug -->
-                        <div class="form-group">
-                            <label for="post_slug">Slug</label>
-                            <input type="text" id="post_slug" name="slug" class="form-control">
-                        </div>
-
-                        <<div class="form-group">
-                            <label>Data</label>
-                            <div class="row">
-
-                                <!-- Day -->
-                                <div class="col-4 col-sm-4 col-md-2 mb-2">
-                                    <input type="number" name="day" id="post_day" class="form-control"
-                                        placeholder="01" min="1" max="31">
-                                </div>
-
-                                <!-- Month -->
-                                <div class="col-8 col-sm-4 col-md-2 mb-2">
-                                    <select name="month" id="post_month" class="form-control">
-                                        <option value="1">Gen</option>
-                                        <option value="2">Feb</option>
-                                        <option value="3">Mar</option>
-                                        <option value="4">Apr</option>
-                                        <option value="5">Mag</option>
-                                        <option value="6">Giu</option>
-                                        <option value="7">Lug</option>
-                                        <option value="8">Ago</option>
-                                        <option value="9">Set</option>
-                                        <option value="10">Ott</option>
-                                        <option value="11">Nov</option>
-                                        <option value="12">Dic</option>
-                                    </select>
-                                </div>
-
-                                <!-- Year -->
-                                <div class="col-6 col-sm-4 col-md-2 mb-2">
-                                    <input type="number" name="year" id="post_year" class="form-control"
-                                        placeholder="2025" min="2023">
-                                </div>
-
-                                <!-- "alle" label -->
-                                <div
-                                    class="col-6 col-sm-2 col-md-1 mb-2 d-flex align-items-center justify-content-center">
-                                    alle
-                                </div>
-
-                                <!-- Hour -->
-                                <div class="col-6 col-sm-4 col-md-2 mb-2">
-                                    <input type="number" name="hour" id="post_hour" class="form-control"
-                                        min="0" max="23" placeholder="06">
-                                </div>
-
-                                <!-- Minute -->
-                                <div class="col-6 col-sm-4 col-md-2 mb-2">
-                                    <input type="number" name="minute" id="post_minute" class="form-control"
-                                        min="0" max="59" placeholder="30">
-                                </div>
-
-                            </div>
-                </div>
-
-
-                <!-- Status -->
-                <div class="form-group">
-                    <label for="post_status">Status</label>
-                    <select id="post_status" name="status" class="form-control">
-                        <option value="published">Published</option>
-                        <option value="draft">Draft</option>
-                    </select>
-                </div>
-
-                <!-- Categories (Example Static List) -->
-                <div class="form-group">
-                    <label for="categories">Categorie</label>
-                    <div>
-                        <!-- Replace with dynamic generation as needed -->
-                        <label>
-                            <input type="checkbox" name="categories[]" value="1">
-                            Archivio Sondaggi
-                        </label><br>
-                        <label>
-                            <input type="checkbox" name="categories[]" value="2">
-                            Featured
-                        </label><br>
-                        <label>
-                            <input type="checkbox" name="categories[]" value="3">
-                            Fotoalbum
-                        </label><br>
-                        <label>
-                            <input type="checkbox" name="categories[]" value="4">
-                            Fotogallery
-                        </label><br>
-                        <label>
-                            <input type="checkbox" name="categories[]" value="5">
-                            Stagione 2015-16
-                        </label><br>
-                        <label>
-                            <input type="checkbox" name="categories[]" value="6">
-                            Stagione 2016-17
-                        </label><br>
-                        <label>
-                            <input type="checkbox" name="categories[]" value="7">
-                            Stagione 2017-18
-                        </label>
-                    </div>
-                </div>
-
-                <!-- Tags -->
-                <div class="form-group">
-                    <label for="post_tags">Tag</label>
-                    <textarea id="post_tags" name="tags" class="form-control" placeholder="Separa i tag con delle virgole"></textarea>
-                </div>
-
-                <!-- Submit -->
-                <button type="submit" class="btn btn-primary">Save Changes</button>
-                </form>
-
-            </div>
-        </div>
-    </div>
-    </div>
 
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -163,25 +31,6 @@
         $(document).on('click', '.quick-edit-btn', function() {
             var $btn = $(this);
             var postId = $btn.data('id');
-            var postName = $btn.data('name');
-            var postSlug = $btn.data('slug');
-            var postDate = $btn.data('date');
-            var postHour = $btn.data('hour');
-            var postMinute = $btn.data('minute');
-            var postStatus = $btn.data('status');
-
-            // Set the form action URL (adjust the URL to your route)
-            $('#quickEditForm').attr('action', '/posts/' + postId + '/quick-edit');
-
-            // Populate form fields with data from the button
-            $('#post_id').val(postId);
-            $('#post_name').val(postName);
-            $('#post_slug').val(postSlug);
-            $('#post_date').val(postDate);
-            $('#post_hour').val(postHour);
-            $('#post_minute').val(postMinute);
-            $('#post_status').val(postStatus);
-
 
             // Open the modal
             $('#quickEditModal').modal('show');
