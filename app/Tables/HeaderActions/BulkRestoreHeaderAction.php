67018a094febdad53fd8ba3070fc82f3428d3045
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-03 13:57:18 +0200

    Update BulkRestoreHeaderAction.php

--- app/Tables/HeaderActions/BulkRestoreHeaderAction.php
+++ app/Tables/HeaderActions/BulkRestoreHeaderAction.php
@@ -8,27 +8,61 @@ class BulkRestoreHeaderAction extends HeaderAction
 {
     protected array $options = [];
 
+    /**
+     * Make a new header action.
+     *
+     * @param string $name
+     * @return static
+     */
     public static function make(string $name = 'restore'): static
     {
         return new static($name);
     }
 
+    /**
+     * Set additional options.
+     */
     public function setOptions(array $options): static
     {
         $this->options = $options;
         return $this;
     }
 
+    /**
+     * Retrieve an option.
+     */
     public function getOption(string $key, $default = null)
     {
         return $this->options[$key] ?? $default;
     }
 
+    /**
+     * Get the URL for the bulk restore action.
+     */
+    public function getUrl(): string
+    {
+        return $this->getOption('link', '#');
+    }
+
+    /**
+     * Get the label for the bulk restore action.
+     */
+    public function getLabel(): string
+    {
+        return $this->getOption('label', 'Bulk Restore');
+    }
+
+    /**
+     * Render the header action.
+     */
     public function render(): string
     {
-        $link  = $this->getOption('link', '#');
-        $label = $this->getOption('label', 'Bulk Restore');
-
-        return '<a href="' . $link . '" class="btn btn-sm btn-primary" data-custom-bulk-restore="true">' . $label . '</a>';
+        $url = $this->getUrl();
+        $label = $this->getLabel();
+        // Return a button with the data-action attribute set to the header action name
+        // and data-href set to your custom URL.
+        return '<button class="btn action-item" tabindex="0" type="button" data-action="' . $this->getName() . '" data-href="' . $url . '">
+                    <span>' . $label . '</span>
+                </button>';
     }
 }
