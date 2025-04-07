68f892117c5e83d3a7440817faace6b2005618f7
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2025-04-07 10:02:01 +0200

    c

--- resources/views/polls/edit.blade.php
+++ resources/views/polls/edit.blade.php
@@ -2,29 +2,34 @@
 
 @section('content')
     <div class="container">
-        <h1>Edit Poll</h1>
+        <h1>Modifica Sondaggio</h1>
         <form method="POST" action="{{ route('polls.update', $poll->id) }}">
             @csrf
             @method('PUT')
 
             <div class="form-group">
-                <label for="question">Question</label>
+                <label for="question">Domanda</label>
                 <input type="text" class="form-control" id="question" name="question" value="{{ old('question', $poll->question) }}" required>
             </div>
 
             <div class="form-group" id="options-container">
-                <label for="options">Options</label>
+                <label for="options">Opzioni</label>
                 @foreach($poll->options as $option)
                     <input type="text" class="form-control mb-2" name="options[]" value="{{ old('options.' . $loop->index, $option->option) }}" required>
                 @endforeach
             </div>
 
             <div class="row mb-3 p-2">
-                <button type="button" class="col-6 btn btn-secondary mb-3" onclick="addOption()">Add another option</button>
+                <button type="button" class="col-6 btn btn-secondary mb-3" onclick="addOption()">Aggiungi un'altra opzione</button>
+            </div>
+
+            <div class="form-group">
+                <label for="min_choices">L'utente deve votare almeno per:</label>
+                <input type="number" class="form-control" id="min_choices" name="min_choices" min="1" value="{{ old('min_choices', $poll->min_choices ?? 1) }}" required>
             </div>
 
             <div class="row p-2">
-                <button type="submit" class="col-12 btn btn-primary">Update Poll</button>
+                <button type="submit" class="col-12 btn btn-primary">Aggiorna Sondaggio</button>
             </div>
 
         </form>
