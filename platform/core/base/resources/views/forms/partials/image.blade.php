9d728708a1d4a14185462fb0f46919018322d064
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-07 09:34:35 +0200

    1

--- platform/core/base/resources/views/forms/partials/image.blade.php
+++ platform/core/base/resources/views/forms/partials/image.blade.php
@@ -1,5 +1,4 @@
 @php
     $allowThumb = Arr::get($attributes, 'allow_thumb', true);
 @endphp
-@dd($allowThumb, $name, $value);
 <x-core::form.image :allow-thumb="$allowThumb" :name="$name" :value="$value" action="select-image" />
