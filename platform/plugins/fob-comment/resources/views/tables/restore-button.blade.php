7522d17349828fa91a44cfb197f5fd293bfd20f8
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-16 10:35:16 +0200

    Update restore-button.blade.php

--- platform/plugins/fob-comment/resources/views/tables/restore-button.blade.php
+++ platform/plugins/fob-comment/resources/views/tables/restore-button.blade.php
@@ -1,5 +1,5 @@
 <x-core::button type="button" color="success" class="restore-btn btn-icon" size="sm" data-bs-toggle="tooltip"
     data-bs-original-title="{{ trans('plugins/fob-comment::comment.restore') }}"
-    data-url="{{ route('restore', $action->getItem()->getKey()) }}">
+    data-url="{{ route('fob-comment.comments.restore', $action->getItem()->getKey()) }}">
     <x-core::icon name="ti ti-refresh" />
 </x-core::button>
