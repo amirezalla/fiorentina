bfccaee3b42d60ece944ad7878b57c68782097a7
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-17 17:34:54 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -199,7 +199,7 @@ class AppServiceProvider extends ServiceProvider
                     })
                     ->get();
                 $video_files = $mediaFiles->map(function ($item) {
-                    return url('storage/' . $item->url);
+                    return \Illuminate\Support\Facades\Storage::temporaryUrl($item->url, now()->addMinutes(15));
                 });
                 $video_file_urls = $mediaFiles->map(function ($item) {
                     return $item->pivot->url;
