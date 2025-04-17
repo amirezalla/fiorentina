321db2e14a1c7f3ba3200c0b22c6d047324cdc71
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-17 12:52:13 +0200

    1

--- resources/views/diretta/includes/modify-chat.blade.php
+++ resources/views/diretta/includes/modify-chat.blade.php
@@ -1,31 +1,58 @@
-@php
-    use Botble\Member\Models\Member;
-@endphp
+@php use Botble\Member\Models\Member; @endphp
+
 <div class="container mt-3">
-
-    @foreach ($chats as $chat)
-        @if (!$loop->first)
-            <div class="commentary-row">
-                <div style="flex: 0.5">
+    <div class="table-responsive">
+        <table class="table table-sm table-striped align-middle">
+            <thead class="table-light">
+                <tr>
                     @if (Str::contains(request()->url(), '/chat-view'))
-                        <a style="margin-right: 5px" href="/delete-chat?id={{ $chat->id }}"><i
-                                class="text-danger fa-solid fa-trash"></i></a>
-                        <a href="/modify-commentary?id={{ $chat->id }}"><i
-                                class="text-white fa-solid fa-pen-to-square"></i></i></a>
+                        <th style="width:70px;">Actions</th>
                     @endif
-                </div>
-                <div class="user p-2">
-                    @php $user=Member::find($chat['user_id']); @endphp
+                    <th>User</th>
+                    <th>Message</th>
+                    <th>Date&nbsp;/&nbsp;Time</th>
+                </tr>
+            </thead>
 
-                    <a class="text-white text-underline"
-                        href="https://laviola.collaudo.biz/admin/members/edit/{{ $user->id }}">
-                        {{ $user->first_name }} {{ $user->last_name }} </a>
+            <tbody>
+                @foreach ($chats as $chat)
+                    @continue($loop->first) {{-- skip the very first chat if that’s intentional --}}
+                    @php
+                        $user = Member::find($chat['user_id']);
+                    @endphp
 
-                </div>
-                <div class="comment-text">{{ $chat['message'] }}
-                </div>
-                <div class="text-white p-2">{{ $chat['created_at'] }}</div>
-            </div>
-        @endif
-    @endforeach
+                    <tr>
+                        {{-- ACTION BUTTONS – only in chat‑view --}}
+                        @if (Str::contains(request()->url(), '/chat-view'))
+                            <td>
+                                <a href="/delete-chat?id={{ $chat->id }}" class="text-danger me-2" aria-label="Delete">
+                                    <i class="fa-solid fa-trash"></i>
+                                </a>
+                                <a href="/modify-commentary?id={{ $chat->id }}" class="text-muted" aria-label="Edit">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                </a>
+                            </td>
+                        @endif
+
+                        {{-- USER --}}
+                        <td>
+                            @if ($user)
+                                <a href="{{ url("admin/members/edit/{$user->id}") }}" class="text-decoration-underline">
+                                    {{ $user->first_name }} {{ $user->last_name }}
+                                </a>
+                            @else
+                                <em class="text-muted">Unknown user</em>
+                            @endif
+                        </td>
+
+                        {{-- MESSAGE --}}
+                        <td>{{ $chat['message'] }}</td>
+
+                        {{-- DATE --}}
+                        <td class="text-nowrap">{{ $chat['created_at'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
