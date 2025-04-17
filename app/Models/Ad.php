9125e9470f0c84a9919b0e49fd5e9012bac064be
Amirezalla <allahverdiamirreza@gmail.com>
2025-04-17 11:49:09 +0200

    Update Ad.php

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -232,7 +232,7 @@ class Ad extends BaseModel
         if ($ads->has(self::GROUP_BACKGROUND_PAGE)) {
             $backgroundAdHtml = view('ads.includes.background-page', ['ad' => $ads->get(self::GROUP_BACKGROUND_PAGE)])->render();
             $content = preg_replace(
-                '/(<div[^>]*class="section".*?>)/i',
+                '/(<section[^>]*class="section".*?>)/i',
                 $backgroundAdHtml . "$1",
                 $content
             );
