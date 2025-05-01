e5410fae7af4517131bdc5984314309966e52bee
Amirezalla <allahverdiamirreza@gmail.com>
2025-05-02 00:01:55 +0330

    Update LineupController.php

--- app/Http/Controllers/LineupController.php
+++ app/Http/Controllers/LineupController.php
@@ -12,21 +12,23 @@ class LineupController extends Controller
         $raw   = json_decode($match->lineups ?? '[]');
         $all   = collect($raw)->groupBy('FORMATION_NAME');
 
-        // the same grouping logic you have in Blade
-        $fiorentina = $all->only([
-            'Fiorentina Subs',
-            'Fiorentina Coach',
-            'Fiorentina Initial Lineup',
-        ]);
-
-        $another    = $all->only([
-            'Another Subs',
-            'Another Coach',
-            'Another Initial Lineup',
-        ]);
+        $fiorentinaLineups = $lineups
+        ->filter(fn ($l) => in_array($l->formation_name, [
+            'Fiorentina Subs', 'Fiorentina Coach', 'Fiorentina Initial Lineup',
+        ]))
+        ->groupBy('formation_name');
+    
+    $anotherTeamLineups = $lineups
+        ->filter(fn ($l) => in_array($l->formation_name, [
+            'Another Subs', 'Another Coach', 'Another Initial Lineup',
+        ]))
+        ->groupBy('formation_name');
 
         $home = json_decode($match->home_team, true);   // ['name', 'id', 'slug', ...]
 $away = json_decode($match->away_team, true);
+
+
+
 
 $isHomeFiorentina = strcasecmp($home['name'], 'Fiorentina') === 0;
 $isAwayFiorentina = strcasecmp($away['name'], 'Fiorentina') === 0;
@@ -34,8 +36,8 @@ $isAwayFiorentina = strcasecmp($away['name'], 'Fiorentina') === 0;
 return view('ads.includes.formazioni-tabs', [
     'isHomeFiorentina'   => $isHomeFiorentina,
     'isAwayFiorentina'   => $isAwayFiorentina,
-    'fiorentinaLineups'  => $fiorentina,
-    'anotherTeamLineups' => $another,
+    'fiorentinaLineups'  => $fiorentinaLineups,
+    'anotherTeamLineups' => $anotherTeamLineups,
 ]);
 
     }
