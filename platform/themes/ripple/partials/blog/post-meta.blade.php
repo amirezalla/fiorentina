dae61aafd54155af83872ce70c4aab43a44ac4ed
Amirezalla <allahverdiamirreza@gmail.com>
2025-05-29 11:14:53 +0330

    Update post-meta.blade.php

--- platform/themes/ripple/partials/blog/post-meta.blade.php
+++ platform/themes/ripple/partials/blog/post-meta.blade.php
@@ -27,7 +27,7 @@
                 <a class="social-btn facebook"
                     href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(request()->fullUrl()) }}"
                     target="_blank" rel="noopener" aria-label="Condividi su Facebook">
-                    <img src="{{ asset('storage/Facebook_logo.png') }}" alt="Facebook">
+                    <img src="{{ asset('storage/Facbook_logo.png') }}" alt="Facebook">
                 </a>
 
                 {{-- X (Twitter) – black logo, grey background --}}
