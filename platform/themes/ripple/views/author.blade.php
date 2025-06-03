716ac1b5f2551f0ae5927cc15bf2acc5caab86e6
Amirezalla <allahverdiamirreza@gmail.com>
2025-06-03 14:30:47 +0200

    Update author.blade.php

--- platform/themes/ripple/views/author.blade.php
+++ platform/themes/ripple/views/author.blade.php
@@ -1,5 +1,9 @@
     {{-- everything here replaces {!! Theme::content() !!} --}}
     {{-- Header section mimicking the screenshot --}}
+    @php
+        use Botble\Media\Models\MediaFile;
+
+    @endphp
     @php Theme::set('section-name', $user->first_name) @endphp
 
     <div class="text-center py-60">
@@ -13,8 +17,10 @@
 
             </div>
         </div>
-        <img src="{{ Theme::asset()->url('images/viola-it-round.svg') }}" alt="{{ $user->first_name }}" width="160"
-            class="mb-30">
+        @php
+            $img = RvMedia::getImageUrl($user->avatar, $user->first_name, 'thumbnail');
+        @endphp
+        <img src=" {{ $img }} " alt="{{ $user->first_name }}" width="160" class="mb-30">
         <h1 class="display-5 font-weight-bold">{{ $user->first_name }} {{ $user->last_name }}</h1>
 
     </div>
