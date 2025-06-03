74f4bc73a5039e1aced472bc67cbec98f3a3bcee
Amirezalla <allahverdiamirreza@gmail.com>
2025-06-03 14:14:08 +0200

    Update author.blade.php

--- platform/themes/ripple/views/author.blade.php
+++ platform/themes/ripple/views/author.blade.php
@@ -1,4 +1,4 @@
-@extends(Theme::getLayout('default')) {{-- uses layout/default.blade.php --}}
+@extends(Theme::getThemeName()) {{-- uses layout/default.blade.php --}}
 
 @section('content')
     {{-- everything here replaces {!! Theme::content() !!} --}}
