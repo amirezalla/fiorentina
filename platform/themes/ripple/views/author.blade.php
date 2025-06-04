d4b6abe6839ed4986cc1bdad2f5afa4cd10ec3a6
Amirezalla <allahverdiamirreza@gmail.com>
2025-06-04 10:45:59 +0200

    Update author.blade.php

--- platform/themes/ripple/views/author.blade.php
+++ platform/themes/ripple/views/author.blade.php
@@ -39,102 +39,107 @@
         $minMainPostsLimit = intval(5);
         $mainPostsLimit = intval(50);
     @endphp
-    <div class="post-group post-group--single">
-        <div class="post-group__content">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-12">
-                    @forelse ($posts as $index => $post)
-                        <article class="post post__vertical post__vertical--single post-item"
-                            style="display: {{ $index < $minMainPostsLimit ? 'flex' : 'none' }}; align-items: center; margin-bottom: 5px;">
-                            <!-- Image on the left -->
-                            <div class="post__thumbnail" style="flex: 1.5; width: 48%;">
-                                {{ RvMedia::image(
-                                    $post->image,
-                                    $post->name,
-                                    'medium',
-                                    attributes: [
-                                        'loading' => 'lazy',
-                                    ],
-                                ) }}
-                                <a class="post__overlay" href="{{ $post->url }}" title="{{ $post->name }}"></a>
-                            </div>
+    <div class="row">
 
-                            <!-- Content (Title and Description) on the right -->
-                            <div class="post__content-wrap" style="flex: 2.5; padding-left: 20px;">
-                                <header class="post__header">
-                                    @php
 
-                                        $date = $post->created_at;
-
-                                        if ($date->isToday()) {
-                                            // If the post was created today, show only hour and minute
-                                            $formattedDate = $date->format('H:i');
-                                        } elseif ($date->isYesterday()) {
-                                            // If it was yesterday, show "Ieri alle" followed by hour and minute
-                                            $formattedDate = 'Ieri alle ' . $date->format('H:i');
-                                        } else {
-                                            // Otherwise, show the day, abbreviated month (in Italian), and hour:minute
-                                            // Set locale to Italian for month names (ensure you have installed the appropriate locale)
-                                            $formattedDate = $date->locale('it')->translatedFormat('d M H:i');
-                                        }
-                                    @endphp
-                                    <div class="text-dark mb-1">
-
-                                        @php
-                                            $categoryName = $post->categories->count()
-                                                ? strtoupper($post->categories->first()->name)
-                                                : 'NOTIZIE';
-                                        @endphp
-
-                                        <span class=" mb-1">
-                                            <span class="post__last4-badge">
-                                                {{ $categoryName }}</span> /
-                                        </span>
-
-                                        <span class="post__date">
-                                            {{ $formattedDate }}
-                                        </span>
-                                        @if ($post->in_aggiornamento)
-                                            <span class="post-group__left-red-badge ml-2"><span
-                                                    class='pulse-circle'></span> <span class="text-white">In
-                                                    Aggiornamento</span>
-                                            </span>
-                                        @endif
-
-                                    </div>
-                                    <h4 class="post__title" style="margin: 0;">
-                                        <a href="{{ $post->url }}" title="{{ $post->name }}"
-                                            style="text-decoration: none; color: inherit;">
-                                            {{ $post->name }}
-                                        </a>
-                                    </h4>
-                                </header>
-                                <div class="post__content">
-                                    <p style="margin: 10px 0 0;">{{ $post->description }}</p>
-                                    <span class=" text-dark mt-1 d-block"
-                                        style="font-family: 'Titillium Web', sans-serif; font-weight: 600; font-size: 16px;color:#888">
-                                        @php
-                                            $post->comments_count = FriendsOfBotble\Comment\Models\Comment::where(
-                                                'reference_id',
-                                                $post->id,
-                                            )->count();
-                                        @endphp
-                                        Di <span class=" fw-bold author-post"
-                                            style="color:#8424e3">{{ $post->author->first_name }}
-                                            {{ $post->author->last_name }}</span> /
-                                        <a class="fw-bold" href="{{ $post->url }}#comments" style="color:#8424e3">
-                                            <i class="fa fa-comment" aria-hidden="true"></i>
-                                            {{ $post->comments_count > 0 ? $post->comments_count : 'Commenta' }}
-                                        </a>
-                                    </span>
+        <div class="post-group post-group--single">
+            <div class="post-group__content">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-12">
+                        @forelse ($posts as $index => $post)
+                            <article class="post post__vertical post__vertical--single post-item"
+                                style="display: {{ $index < $minMainPostsLimit ? 'flex' : 'none' }}; align-items: center; margin-bottom: 5px;">
+                                <!-- Image on the left -->
+                                <div class="post__thumbnail" style="flex: 1.5; width: 48%;">
+                                    {{ RvMedia::image(
+                                        $post->image,
+                                        $post->name,
+                                        'medium',
+                                        attributes: [
+                                            'loading' => 'lazy',
+                                        ],
+                                    ) }}
+                                    <a class="post__overlay" href="{{ $post->url }}" title="{{ $post->name }}"></a>
                                 </div>
 
-                        </article>
-                    @empty
-                        <p class="text-center text-muted">
-                            {{ __('Nessun articolo pubblicato al momento.') }}
-                        </p>
-                    @endforelse
+                                <!-- Content (Title and Description) on the right -->
+                                <div class="post__content-wrap" style="flex: 2.5; padding-left: 20px;">
+                                    <header class="post__header">
+                                        @php
+
+                                            $date = $post->created_at;
+
+                                            if ($date->isToday()) {
+                                                // If the post was created today, show only hour and minute
+                                                $formattedDate = $date->format('H:i');
+                                            } elseif ($date->isYesterday()) {
+                                                // If it was yesterday, show "Ieri alle" followed by hour and minute
+                                                $formattedDate = 'Ieri alle ' . $date->format('H:i');
+                                            } else {
+                                                // Otherwise, show the day, abbreviated month (in Italian), and hour:minute
+                                                // Set locale to Italian for month names (ensure you have installed the appropriate locale)
+                                                $formattedDate = $date->locale('it')->translatedFormat('d M H:i');
+                                            }
+                                        @endphp
+                                        <div class="text-dark mb-1">
+
+                                            @php
+                                                $categoryName = $post->categories->count()
+                                                    ? strtoupper($post->categories->first()->name)
+                                                    : 'NOTIZIE';
+                                            @endphp
+
+                                            <span class=" mb-1">
+                                                <span class="post__last4-badge">
+                                                    {{ $categoryName }}</span> /
+                                            </span>
+
+                                            <span class="post__date">
+                                                {{ $formattedDate }}
+                                            </span>
+                                            @if ($post->in_aggiornamento)
+                                                <span class="post-group__left-red-badge ml-2"><span
+                                                        class='pulse-circle'></span> <span class="text-white">In
+                                                        Aggiornamento</span>
+                                                </span>
+                                            @endif
+
+                                        </div>
+                                        <h4 class="post__title" style="margin: 0;">
+                                            <a href="{{ $post->url }}" title="{{ $post->name }}"
+                                                style="text-decoration: none; color: inherit;">
+                                                {{ $post->name }}
+                                            </a>
+                                        </h4>
+                                    </header>
+                                    <div class="post__content">
+                                        <p style="margin: 10px 0 0;">{{ $post->description }}</p>
+                                        <span class=" text-dark mt-1 d-block"
+                                            style="font-family: 'Titillium Web', sans-serif; font-weight: 600; font-size: 16px;color:#888">
+                                            @php
+                                                $post->comments_count = FriendsOfBotble\Comment\Models\Comment::where(
+                                                    'reference_id',
+                                                    $post->id,
+                                                )->count();
+                                            @endphp
+                                            Di <span class=" fw-bold author-post"
+                                                style="color:#8424e3">{{ $post->author->first_name }}
+                                                {{ $post->author->last_name }}</span> /
+                                            <a class="fw-bold" href="{{ $post->url }}#comments"
+                                                style="color:#8424e3">
+                                                <i class="fa fa-comment" aria-hidden="true"></i>
+                                                {{ $post->comments_count > 0 ? $post->comments_count : 'Commenta' }}
+                                            </a>
+                                        </span>
+                                    </div>
+
+                            </article>
+                        @empty
+                            <p class="text-center text-muted">
+                                {{ __('Nessun articolo pubblicato al momento.') }}
+                            </p>
+                        @endforelse
+                    </div>
                 </div>
             </div>
         </div>
