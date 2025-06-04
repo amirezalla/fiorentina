ff4cfc00372ea2ab7c471c3d0991c657f3313389
Amirezalla <allahverdiamirreza@gmail.com>
2025-06-04 12:09:04 +0200

    Update post-meta.blade.php

--- platform/themes/ripple/partials/blog/post-meta.blade.php
+++ platform/themes/ripple/partials/blog/post-meta.blade.php
@@ -14,7 +14,7 @@
             </span>
             @if ($post->author->avatar->url)
                 @php
-                    $img = RvMedia::image($post->author->avatar->url, $post->author->first_name, 'thumbnail', [
+                    $img = RvMedia::image($post->author->avatar->url, $post->author->first_name, 'thumbnail', true, [
                         'style' => 'border-radius:50%;width:25px;height:25px;',
                     ]);
                 @endphp
