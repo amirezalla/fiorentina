6b746f80ec7e0f3b723b201d559316ce43673dbc
Amirezalla <allahverdiamirreza@gmail.com>
2025-06-09 11:38:20 +0200

    Update StandingController.php

--- app/Http/Controllers/StandingController.php
+++ app/Http/Controllers/StandingController.php
@@ -27,7 +27,7 @@ class StandingController extends Controller
             Standing::truncate();
     
             $response = Http::withHeaders([
-                'X-Auth-Token' => '1e9b76550emshc710802be81e3fcp1a0226jsn069e6c35a2bb' 
+                'X-Auth-Token' => 'e1ef65752c2b42c2b8002bccec730215' 
             ])->get('http://api.football-data.org/v4/competitions/SA/standings');
     
             if ($response->successful()) {
@@ -70,7 +70,7 @@ class StandingController extends Controller
         $latestUpdate = Matches::where('status', 'TIMED')->latest('updated_at')->first();
         if (!$latestUpdate || $latestUpdate->updated_at <= Carbon::now()->subHours(20)) {
         $response = Http::withHeaders([
-            'X-Auth-Token' => '1e9b76550emshc710802be81e3fcp1a0226jsn069e6c35a2bb'
+            'X-Auth-Token' => 'e1ef65752c2b42c2b8002bccec730215'
         ])->get('https://api.football-data.org/v4/teams/99/matches', [
             'status' => 'SCHEDULED'
         ]);
