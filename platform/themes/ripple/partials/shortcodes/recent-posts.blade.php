107e4f7c2ed722db3a3f4670fab3b87c3fc41394
Amirezalla <allahverdiamirreza@gmail.com>
2025-06-12 09:49:58 +0200

    Update recent-posts.blade.php

--- platform/themes/ripple/partials/shortcodes/recent-posts.blade.php
+++ platform/themes/ripple/partials/shortcodes/recent-posts.blade.php
@@ -55,7 +55,7 @@
                                                 {{ RvMedia::image(
                                                     $post->image,
                                                     $post->name,
-                                                    '_thumbnail',
+                                                    'thumbnail',
                                                     attributes: [
                                                         'loading' => 'lazy',
                                                     ],
