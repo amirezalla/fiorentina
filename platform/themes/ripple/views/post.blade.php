640987171b9026cbc22a4495e6be57dea7c54838
Amirezalla <allahverdiamirreza@gmail.com>
2025-07-20 13:18:46 +0200

    Update post.blade.php

--- platform/themes/ripple/views/post.blade.php
+++ platform/themes/ripple/views/post.blade.php
@@ -56,7 +56,7 @@
     <br>
     {!! apply_filters(BASE_FILTER_PUBLIC_COMMENT_AREA, null, $post) !!}
     </div>
-    </div>
+
     <div class="col-lg-4">
         {!! Theme::partial('sidebar') !!}
     </div>
