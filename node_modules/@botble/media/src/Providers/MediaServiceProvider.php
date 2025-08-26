46cc59c9aa2c7fdc23497c81757be95efb15645e
Amirezalla <allahverdiamirreza@gmail.com>
2025-08-26 12:29:04 +0200

    Revert "MediaServiceProvider"
    This reverts commit a75196c2d707b8854331d5141705ec309150b618.

--- node_modules/@botble/media/src/Providers/MediaServiceProvider.php
+++ node_modules/@botble/media/src/Providers/MediaServiceProvider.php
@@ -74,7 +74,7 @@ class MediaServiceProvider extends ServiceProvider
             ->publishAssets();
 
         Storage::extend('wasabi', function ($app, $config) {
-            $config['url'] = 'https://laviols3.collaudo.biz';
+            $config['url'] = 'https://' . $config['bucket'] . '.s3.' . $config['region'] . '.wasabisys.com/';
 
             $client = new S3Client([
                 'endpoint' => $config['url'],
