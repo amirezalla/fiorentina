6c99733870e758155991295511a732d96d25e556
Amirezalla <allahverdiamirreza@gmail.com>
2025-09-06 13:21:17 +0200

    Update StandingController.php

--- app/Http/Controllers/StandingController.php
+++ app/Http/Controllers/StandingController.php
@@ -551,7 +551,6 @@ foreach($data as $tournament){
                     if (!$startTs || $startTs < $fromTs) {
                         continue;
                     }
-                    dd($startTs, $match['START_TIME'],$fromTs);
 
                     // Human-readable local time for storage
                     $matchDate = Carbon::createFromTimestamp($startTs, 'Europe/Rome')->toDateTimeString();
