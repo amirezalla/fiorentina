fdb862276424441084b991485fe2cf9c900fe101
Amirezalla <allahverdiamirreza@gmail.com>
2025-09-06 10:50:44 +0200

    1

--- resources/views/ads/includes/dblog-p2.blade.php
+++ resources/views/ads/includes/dblog-p2.blade.php
@@ -8,7 +8,7 @@
 
             @endphp
 
-            <div class="row justify-content-center mx-0">
+            <div class="row justify-content-center mx-0 mb-2">
                 <div class="col-12 mx-auto p-0">
                     {{-- Link through your trackClick route so that a click is counted --}}
                     <a href="{{ route('ads.click', ['id' => $ad->id]) }}" class="d-block">
@@ -19,7 +19,7 @@
             </div>
         @else
             {{-- For type=2 (Google Ad Manager or custom HTML), no impression is tracked here --}}
-            <div class="row justify-content-left mx-0">
+            <div class="row justify-content-left mx-0 mb-2">
                 {!! $ad->amp !!}
             </div>
         @endif
