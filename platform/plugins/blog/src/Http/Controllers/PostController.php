c81edee53221cabd37d9adbaaf47c0eea47a18c9
Amirezalla <allahverdiamirreza@gmail.com>
2025-10-16 22:32:09 +0200

    Update PostController.php

--- platform/plugins/blog/src/Http/Controllers/PostController.php
+++ platform/plugins/blog/src/Http/Controllers/PostController.php
@@ -80,6 +80,7 @@ class PostController extends BaseController
             ->map(fn ($id) => (int) $id)
             ->unique()
             ->all();
+        dd($ids);
 
         $post->collaborators()->sync($ids);
 
