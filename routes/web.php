90ff0f9f7f20aa85277163e9d684ed44f3993d31
Amirezalla <allahverdiamirreza@gmail.com>
2025-10-16 13:01:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -518,3 +518,5 @@ Route::get('/test-mailgun', function () {
 
     return 'Sent (check logs & Mailgun dashboard).';
 });
+
+Route::get('/formazione-nextmatch', [FormazioneController::class, 'index'])->name('formazione.index');
